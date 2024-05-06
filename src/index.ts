fa288dc90d32a77293565cff28e2dbac04065ec6
Lana ON23B <151159635+LanaOnlinemedien@users.noreply.github.com>
2024-05-06 16:57:37 +0200

    implementation localStorage in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import './styles/styles.css';
 import { form, displayDiv, totalValueDiv } from "./dom-utils.ts";
+import {Book, getBooks, saveBooks} from "./storage.ts";
 
+//Funktionen werden an Anfang ausgeführt, damit gespeicherte Bücher und der Wert des Bücherregals direkt angezeigt werden und nicht erst bei Klicken des submit-Buttons
+showBooks();
+showTotalValue();
+
+//submit-Button bekommt ein Event zugewiesen
 form.addEventListener('submit', function (event: Event) {
+        //verhindert, dass Neuladen der Seite bei Klicken des submit-Buttons
         event.preventDefault();
         const title = (document.getElementById('title') as HTMLInputElement).value;
         const author = (document.getElementById('author') as HTMLInputElement).value;
@@ -9,24 +16,51 @@ form.addEventListener('submit', function (event: Event) {
         const notes = (document.getElementById('notes') as HTMLTextAreaElement).value;
         const ranking = parseInt((document.getElementById('ranking') as HTMLSelectElement).value);
 
-        showBooks({title, author, price, notes, ranking});
-        showTotalValue(price);
+        //Objekt für neues Buch wird mit den oben extrahierten Daten erstellt. Datenstruktur wie in zuvor definiertem Interface Book 
+        const newBook: Book = {
+            title: title,
+            author: author,
+            price: price,
+            notes: notes,
+            ranking: ranking,
+        }
+
+        //neues Buch wird gespeichert
+        saveBooks(newBook);
+        //alle Bücher werden angezeigt
+        showBooks();
+        //Gesamtwert des Bücherregals, aller abgespeicherten Bücher, wird angezeigt
+        showTotalValue();
+        //Formular mit allen Eingabefeldern wird zurückgesetzt
+        form.reset()
 });
 
-function showBooks(book: { title: string; author: string; price: number; notes: string; ranking: number }) {
-    displayDiv.innerHTML += `<div>
-        <h3>${book.title} by ${book.author}</h3>
-        <p>Preis: ${book.price.toFixed(2)}€</p>
-        <p>Anmerkungen: ${book.notes}</p>
-        <p>Sterneranking: ${"★".repeat(book.ranking)}</p>
-    </div>`;
+//Anzeigen der eingegebenen Bücher
+function showBooks() {
+    //Leeren des Inhalts des Containers, damit frühere Inhalte nicht dupliziert werden
+    displayDiv.innerHTML = '';
+    //Holt alle Bücher aus dem Local Storage
+    const books: Book[] = getBooks();
+    //über jedes vorher gespeicherte Buch iterieren und alle folgenden Informationen ausgeben
+    books.forEach((book: Book) => {
+        displayDiv.innerHTML += `<div>
+            <h3>${book.title} von ${book.author}</h3>
+            <p>Preis: ${book.price.toFixed(2)}€</p>
+            <p>Anmerkungen: ${book.notes}</p>
+            <p>Sterneranking: ${"★".repeat(book.ranking)}</p>
+        </div>`;
+    });
 }
 
 
-
-function showTotalValue(price: number): void {
+//Gibt den Gesamtwert des Bücherregals aus
+function showTotalValue(): void {
     let totalValue = 0; // Variable zur Speicherung des Gesamtwertes
-    totalValue += price; // Addiere den Preis des neuen Buches zum Gesamtwert
+    const books: Book[] = getBooks(); //Holt alle Bücher aus dem Local Storage
+    books.forEach((book: Book) => {
+        totalValue += book.price; //Addiert den Preis des Buches zum Gesamtwert
+    });
+    //Gesamtwert des Bücherregals mit 2 Nachkommastellen ausgegeben
     totalValueDiv.textContent = `Gesamtwert des Bücherregals: ${totalValue.toFixed(2)}€`;
 }
 
--- src/storage.ts
+++ src/storage.ts
@@ -1,3 +1,4 @@
+//alle Objekte, die als Buch behandelt werden, müssen folgende 5 Eigenschaften in gegebenem Typen enthalten
 export interface Book {
     title: string;
     author: string;
@@ -6,17 +7,17 @@ export interface Book {
     ranking: number;
 }
 
-export function saveBook(book: Book) {
-    //speichert das Buch in den Local Storage
+//speichert das Buch in den Local Storage
+export function saveBooks(book: Book) {
     // Holen aller Bücher aus dem Local Storage, wenn es noch keine gibt, wird ein leeres Array zurückgegeben
     let books: Book[] = JSON.parse(localStorage.getItem('books') as string) || [];
-    //neues Buch wird hinzugefügt
+    //neues Buch wird hinzugefügt, durch Abspeichern in Array werden zuvor gespeicherte Daten nicht überschrieben, sondern in Array hinzugefügt am Ende
     books.push(book);
     //die alten und neuen Bücher werden wieder in den Local Storage gespeichert
     localStorage.setItem('books', JSON.stringify(books));
 }
 
+//Holt alle Bücher aus dem Local Storage
 export function getBooks(): Book[] {
-    //Holt alle Bücher aus dem Local Storage
     return JSON.parse(localStorage.getItem('books') as string) || [];
 }
